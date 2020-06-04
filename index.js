c9a84e3af331727f386599adc8f4780dfed23cd2
dewerner <werner.huysmans@gmail.com>
2020-06-04 22:39:08 +0200

    implementing workers first step

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ let puntentotaal = 1
 let quizType = 'quiz'
 let olodType = 'eolod'
 let state = '' 
-let io = null
-const socketIO = require('socket.io');
 const credentials = {
 	client: {
 		id: process.env.CLIENTID,
@@ -32,6 +30,9 @@ const credentials = {
 let oauth2 = null
 let authorizationUri = null
 const { check, validationResult } = require('express-validator')
+let Queue = require('bull')
+let REDIS_URL = process.env.REDIS_URL
+let workQueue = new Queue( 'work', REDIS_URL )
 
 app.get('/', ( req, res ) => {
 	res.send('<h2 class="form"><a href="/auth">Login via Canvas</a></h2>')
@@ -96,12 +97,14 @@ app.get('/test', [
 		})
 		// console.log( assignment.data )
 		pointsPossible = parseInt( assignment.data.points_possible )
-		const getSubmissions = async () => {
+		const getResultsFromWorkers = async () => {
 			let keepGoing = true
-			let result = []
+			let results = []
 			let submissionsURL = quizType === 'quiz' ? `${ baseURL }courses/${ courseID }/quizzes/${ assignmentID }/submissions?per_page=50` :
 				`${ baseURL }courses/${ courseID }/assignments/${ assignmentID }/submissions?per_page=50`
 			while ( keepGoing ) {
+				
+				
 				let response = await axios({
 					method: 'get',
 					url: submissionsURL,
@@ -110,8 +113,13 @@ app.get('/test', [
 					}
 				})
 				const resultArray = quizType === 'quiz' ? response.data.quiz_submissions : response.data
-				resultArray.map( ( resultObject ) => {
-					result.push( resultObject )
+				// resultArray.map( ( resultObject ) => {
+				// 	result.push( resultObject )
+				// } )
+				app.post( '/job', async ( req, res ) => {
+					let job = await workQueue.add( { resultArray, token, quizType } )
+					let result = res.json( { result: job.result } )
+					results.push( result )
 				} )
 				let parsed = parse( response.headers.link )
 				if ( parseInt( parsed.current.page ) >= parseInt( parsed.last.page ) ) {
@@ -123,52 +131,9 @@ app.get('/test', [
 			}
 			return result
 		}
-		const progress = setInterval( () => {
-			io.emit( 'time', new Date().toTimeString() )
-		}, 1000 )
-		const result = await getSubmissions()
-		const getAll = async ( data ) => {
-			let rows = []
-			for ( const single_result of data ) {
-				const user_id = single_result.user_id
-				if ( ! single_result.score && ! single_result.entered_grade ) {
-					continue
-				}
-				try {
-					const user_details = await axios( {
-						method: 'get',
-						url: `${ baseURL }users/${ user_id }`,
-						headers: {
-							'Authorization': `Bearer ${ token }`
-						}
-					} )
-					let row = []
-					let points = quizType === 'quiz' ? single_result.score : single_result.entered_grade
-					let correctedScore = recalculateScore( parseFloat( points ) )
-					let afgerondeScore = olodType === 'dolod' ? roundTo( correctedScore, 0.1 ) :
-						roundTo( correctedScore, 1 )
-					row.push( 
-						user_details.data.sortable_name ? user_details.data.sortable_name : 'onbekend',
-						user_details.data.name ? user_details.data.name : 'onbekend', 
-						user_details.data.email ? user_details.data.email : 'onbekend',
-						points,
-						correctedScore,
-						afgerondeScore
-					)
-					rows.push( row )
-					}					
-				catch ( e ) {
-					// res.send( e )
-					console.log(e)
-				}	
-			}
-			return rows
-		}
-		
-		const rows = await getAll( result )
-		clearInterval( progress )
+		const data = await getResultsFromWorkers()
 		// console.log( 'rows', rows )
-		writeExcel( rows )
+		writeExcel( data )
 		res.download( './text.xlsx' )
 		// res.status( 200 ).send( rows )
 	}
@@ -181,26 +146,6 @@ app.get('/test', [
 const getRandomIdent = () => {
 	return Math.random().toString(36).substring(4)
 }
-
-const recalculateScore = ( score ) => {
-	let ces = mcType === 'MC4' ? 0.625 : 0.6667
-	let tellerLeft = score / pointsPossible * puntentotaal
-	let tellerRight = puntentotaal * ces
-	let noemer = puntentotaal - ( puntentotaal * ces )
-	let lastFactor = puntentotaal / 2
-	let herberekendeScore = puntentotaal / 2 + ( tellerLeft - tellerRight ) / noemer * lastFactor
-	let tmp = roundScore( herberekendeScore, 4 )
-	return tmp <= 0 ? 0 : roundScore( tmp, 2 )
-}
-
-const roundScore = ( x, n ) => {
-	let factor = parseFloat( Math.pow( 10, n ) )
-	return Math.trunc( x * factor + 0.5 ) / factor
-}
-
-const roundTo = ( n, to ) => {
-	return to * Math.round( n / to );
- }
 
 const writeExcel = ( rows ) => {
 	console.log( rows.length )
@@ -223,11 +168,9 @@ const writeExcel = ( rows ) => {
 	wb.Sheets[ "Scores" ] = ws
 	// let wbout = XLSX.write( wb, { bookType: 'xlsx', type: 'binary' } )
 	// saveAs( new Blob( [ s2ab( wbout ) ], { type: 'application/octet-stream' } ), 'test.xlsx' )
-	io.emit( 'ready', 'ready' )
 	XLSX.writeFile( wb, 'text.xlsx' )
 }
-
-const server = app.listen( port, () =>  {
+app.listen( port, () =>  {
 	console.log( `listening on port ${ port }` )
 	state = getRandomIdent()
 	oauth2 = require('simple-oauth2').create( credentials )
@@ -240,8 +183,3 @@ const server = app.listen( port, () =>  {
 } )
 
 app.use( '/css', express.static( path.join( __dirname, 'css' ) ) )
-io = socketIO( server )
-io.on( 'connection', ( socket ) => {
-	console.log( 'client connected' )
-	io.emit( 'start-progress', 'start progress' )
-} )
---
+++ worker.js
@@ -0,0 +1,75 @@
+let throng = require( 'throng' )
+let Queue = require('bull')
+const axios = require('axios')
+let port = process.env.PORT || 3000
+let school = process.env.SCHOOL
+
+let REDIS_URL = process.env.REDIS_URL
+let workers = process.env.WEB_CONCURRENCY || 2
+let maxJobsPerWorker = 100
+
+const start = () => {
+	let workQueue = new Queue( 'work', REDIS_URL )
+	workQueue.process( maxJobsPerWorker, async ( job ) => {
+		const resultArray = job.data.resultArray
+		const token = job.data.token
+		const quizType = job.data.quizType
+		let rows = []
+		for ( const single_result of resultArray ) {
+			const user_id = single_result.user_id
+			if ( ! single_result.score && ! single_result.entered_grade ) {
+				continue
+			}
+			try {
+				const user_details = await axios( {
+					method: 'get',
+					url: `${ baseURL }users/${ user_id }`,
+					headers: {
+						'Authorization': `Bearer ${ token }`
+					}
+				} )
+				let row = []
+				let points = quizType === 'quiz' ? single_result.score : single_result.entered_grade
+				let correctedScore = recalculateScore( parseFloat( points ) )
+				let afgerondeScore = olodType === 'dolod' ? roundTo( correctedScore, 0.1 ) :
+					roundTo( correctedScore, 1 )
+				row.push( 
+					user_details.data.sortable_name ? user_details.data.sortable_name : 'onbekend',
+					user_details.data.name ? user_details.data.name : 'onbekend', 
+					user_details.data.email ? user_details.data.email : 'onbekend',
+					points,
+					correctedScore,
+					afgerondeScore
+				)
+				rows.push( row )
+				}					
+			catch ( e ) {
+				// res.send( e )
+				console.log(e)
+			}	
+		}
+		return { result:  rows }
+	} )
+}
+
+const recalculateScore = ( score ) => {
+	let ces = mcType === 'MC4' ? 0.625 : 0.6667
+	let tellerLeft = score / pointsPossible * puntentotaal
+	let tellerRight = puntentotaal * ces
+	let noemer = puntentotaal - ( puntentotaal * ces )
+	let lastFactor = puntentotaal / 2
+	let herberekendeScore = puntentotaal / 2 + ( tellerLeft - tellerRight ) / noemer * lastFactor
+	let tmp = roundScore( herberekendeScore, 4 )
+	return tmp <= 0 ? 0 : roundScore( tmp, 2 )
+}
+
+const roundScore = ( x, n ) => {
+	let factor = parseFloat( Math.pow( 10, n ) )
+	return Math.trunc( x * factor + 0.5 ) / factor
+}
+
+const roundTo = ( n, to ) => {
+	return to * Math.round( n / to );
+}
+
+throng( { workers, start } )
