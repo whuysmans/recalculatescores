b1351e3ffe56c3edd4683a17621b2e85ac562971
dewerner <werner.huysmans@gmail.com>
2020-06-13 13:37:41 +0200

    ok

--- client.js
+++ client.js
@@ -1,11 +1,14 @@
 let progress = 0
 let statusElement = null
+let intervalID = 0
 
-const getResults = async () => {
+const getResults = async ( event ) => {
+	event.preventDefault()
 	const form = document.querySelector( '#resultForm' )
 	const formData = new FormData( form )
 	console.log( formData )
 	let results = await( fetch( '/test', formData ) )
+	clearInterval( intervalID )
 }
 
 const getUpdate = async () => {
@@ -16,6 +19,8 @@ const getUpdate = async () => {
 window.onload = () => {
 	const btn = document.querySelector( '#resultSubmit' )
 	statusElement = document.querySelector( '#status' )
-	btn.prevenDefault()
+	intervalID = setInterval( async () => {
+		await getUpdate()
+	}, 1000 )
 	btn.addEventListener( 'click', getResults  )
 }
