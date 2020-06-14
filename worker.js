521827b2a90967d179315bb298ea35dc73091de1
dewerner <werner.huysmans@gmail.com>
2020-06-14 16:39:33 +0200

    ok

--- worker.js
+++ worker.js
@@ -62,7 +62,8 @@ const getUserDetails = async ( job ) => {
 	let iteration = 0
 	for ( const single_result of result ) {
 		iteration++
-		let progress = result.length / iteration
+		let progress = result.length - parseFloat( result.length / iteration )
+		console.log( progress )
 		job.progress( progress )
 		const user_id = single_result.user_id
 		if ( ! single_result.score && ! single_result.entered_grade ) {
