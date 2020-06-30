57e5bdeb58b9cb47645f28687d79e8b4c2ca8c3d
dewerner <werner.huysmans@gmail.com>
2020-06-30 17:39:53 +0200

    ok

--- worker.js
+++ worker.js
@@ -99,7 +99,7 @@ const getAllData = async ( job ) => {
 				variables.after = response.data.assignment.submissionsConnection.pageInfo.endCursor
 			}
 		} catch ( err ) {
-			// console.log( err )
+			console.log( err )
 		}
 	}
 	return rows 
